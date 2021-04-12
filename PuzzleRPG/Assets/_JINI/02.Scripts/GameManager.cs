f8128dcacb9ec536435631fac029158e7b484866
Jini <wlsdl6685@gmail.com>
2021-04-12 21:34:44 +0900

    블록 스프라이트로 쓰일 이미지 편집, 적용 (15*15) pixel per unit 15로 설정 메인카메라 사이즈 7로 설정 (세로 14유닛) 해상도를 비율에 맞춰 화면에 딱맞게 보이도록 함

---
+++ PuzzleRPG/Assets/_JINI/02.Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        //해상도를 비율에 맞춰 화면에 딱맞게 보이도록 함
+        Screen.SetResolution(Screen.width, Screen.width / 9 * 16, true);
+    }
+}
