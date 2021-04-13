b747c05c4ede5eaf783a23ef36541a4a332c2b20
Jini <wlsdl6685@gmail.com>
2021-04-13 22:30:48 +0900

    블록 배치, 블록 터치 이벤트 테스트 (사라지게 함) Block클래스와 해당 클래스를 상속받는 각각의 색깔블록 클래스 생성

---
+++ PuzzleRPG/Assets/_JINI/02.Scripts/Block.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Block : MonoBehaviour
+{
+    enum STATE { IDLE, WAIT, PANG }
+    STATE state;
+
+
+    private void OnMouseDown()
+    {
+       gameObject.SetActive(false);
+    }
+}
---
+++ PuzzleRPG/Assets/_JINI/02.Scripts/Board.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    static public int x = 7;
+    static public int y = 9;
+    static public int typeSize = 4;
+
+    GameObject[,] blocks = new GameObject[x, y];
+    GameObject[] type = new GameObject[typeSize];
+
+    private void Awake()
+    {
+        for (int i = 0; i < type.Length; i++)
+        {
+            type[i]= Resources.Load("Prefabs/Block_" + i.ToString())as GameObject;
+        }
+       
+    }
+    private void OnEnable()
+    {
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                GameObject block =  Instantiate(type[Random.Range(0,typeSize)]).gameObject;
+                blocks[i, j] = block;
+                block.transform.position = new Vector2(i-3, j-4);
+            }
+        }
+    }
+   
+    private void OnMouseDrag()
+    {
+
+    }
+    private void OnMouseUp()
+    {
+
+    }
+}
